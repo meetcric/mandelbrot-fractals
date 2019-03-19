690dbcaa9ec20aaaa0097a16746367e7f6e80bc7
meetcric <mp32445@gmail.com>
2019-03-20 00:06:08 +0530

    velocity mode added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import {MatRadioModule} from '@angular/material/radio';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'angular-cli';
+  
 }
+
+
--- src/app/fractal-image/fractal-image.component.ts
+++ src/app/fractal-image/fractal-image.component.ts
@@ -16,7 +16,7 @@ var a
   styleUrls: ['./fractal-image.component.css']
 })
 export class FractalImageComponent implements OnInit {
-
+ 
   // {int} Controls the sensitivity of wheel zoom.
   WHEEL_ZOOM_SLUGGISHNESS_PIXELS = 600;
 
@@ -34,171 +34,223 @@ export class FractalImageComponent implements OnInit {
   present_image_properties = null;
   flag = true;
 
+  
+
   // `true` iff the content is being dragged.
   dragging = false;
   motion_x = 0;
   motion_y = 0;
+  motion_z = 0;
+  velocity_x = 0;
+  velocity_y = 0;
+  velocity_z = 0;
 
 
 
-constructor() {
-}
+  /**
+   * Variable needed for Velocity mode
+   */
+  timeouts_per_frame = 2;
+  frame_rate = 25;  // Frames per second (roughly)
+  video_speedup = 1.4;  // Factor by which video will be sped up vs. realtime capture. (Just factors into frame_rate.)
+  MOTION_TIMEOUT = Math.round(1000 / this.frame_rate * this.video_speedup / this.timeouts_per_frame);
+  // {float} Multiplier for velocity.
+  VELOCITY_FACTOR = 1 / this.MOTION_TIMEOUT;  // Move by mouse position every second.
+  // {float} Multiplier for zoom velocity.
+  ZOOM_VELOCITY_FACTOR = this.VELOCITY_FACTOR * 0.6;
+  // {float} Multiplier for acceleration.
+  ACCELERATION_FACTOR = this.VELOCITY_FACTOR / this.MOTION_TIMEOUT; // Every second, increase velocity by mouse position/sec.
+  // {float} Multiplier for zoom acceleration.
+  ZOOM_ACCELERATION_FACTOR = this.ACCELERATION_FACTOR * 0.6;
 
-ngOnInit() {
-  this.set(0, 0, 0.015625, 0.015625, 256, 256, 1225, "cpp", true);
-  this.setParmeter();
-  // Make images interactive.
-  this.imageInteractive();
-}
-
-
-//set the parameter to the desired_image_properties object
-set(x, y, pix_x, pix_y, width, height, max_depth, renderer, darken) {
-  this.desired_image_properties = {
-    x: x,
-    y: y,
-    pix_x: pix_x,
-    pix_y: pix_y,
-    width: width,
-    height: height,
-    max_depth: max_depth,
-    renderer: renderer,
-    darken: darken
+  constructor() {
   }
-  this.present_image_properties = (JSON.parse(JSON.stringify(this.desired_image_properties)));
 
-}
-/**
- * setter and getter method
- */
-
-set zoom_level(v) { this._zoom_level = v; }
-get zoom_level() { return this._zoom_level; }
-
-set scale(v) { this.zoom_level = Math.log(v); }
-get scale() { return Math.exp(this.zoom_level); }
-
-// Scale of 1.0 has size_y of 4.0.
-get image_size_x() { return this.desired_image_properties.pix_x * this.desired_image_properties.width; }
-get image_size_y() { return 4 / this.scale; }
-
-// Pixel x/y are same, based on height.
-get pix_size_x() { return this.pix_size_y; }
-get pix_size_y() { return this.image_size_y / this.desired_image_properties.height; }
-
-/**
- * sets the parameter to the image src
- */
-setParmeter() {
-  var json = JSON.stringify(this.desired_image_properties);
-  var img_url = "http://127.0.1.1:8888/img?json=" + json;
-  // console.log(json)
-  console.log(img_url)
-  $(".image").attr("src", img_url);
-}
-/**
- * Function that responsible for image-interaction
- */
-imageInteractive() {
-  interact(".image")
-    .draggable({
-      inertia: true
-    })
-    .on('up', (e) => {
-      this.endDrag();
-    })
-    .on('dragstart', (e) => {
-     this.dragging = true;
-    })
-    .on('dragend', (e) => {
-      // Clear flag `dragging` to re-enable mouseclicks.
-      // Uses setTimeout to ensure that actions currently firing (on mouseup at the end of a drag)
-      // are still blocked from changing the content.
-      this.endDrag();
-    })
-    .on('dragmove', (e) => {
-      // this.debugMessage(`DragMove: e = ${e}`);
-      // e.preventDefault();
-      if ((e.buttons & this.ZOOM_BUTTON_MASK) != 0) {
-        // Mouse button & drag used for zoom (not currently enabled).
-        let amt = e.dy / this.ZOOM_SLUGGISHNESS;
-        if (this.motion === "position") {
-          this.desired_image_properties.zoomBy(amt);
-        }
-      } else {
-        // Default for position.
-        let dx = e.dx;
-        let dy = e.dy;
-        this.motion_x += dx;
-        this.motion_y += dy;
-        if (this.motion === "position") {
-          this.panBy(dx, dy);
-          
-        }
-      }
-    })
-    //@ts-ignore
-    .on("wheel", (e) => {
-      e.preventDefault();
-      // console.log(e)
-      let sluggishness;
-      if (e.originalEvent.deltaMode == 0) {
-        sluggishness = this.WHEEL_ZOOM_SLUGGISHNESS_PIXELS;
-      }
-      let amt = - e.originalEvent.deltaY / sluggishness;
-      if (this.motion === "position") {
-        this.zoomByAt(amt, (e.offsetX - e.target.clientWidth / 2.0) * this.desired_image_properties.pix_x,
-          (e.offsetY - e.target.clientHeight / 2.0) * this.desired_image_properties.pix_y);
-      }
-    })
-
-}
-zoomByAt(v, offset_x, offset_y) {
-  console.log(1)
-  this.present_image_properties = (JSON.parse(JSON.stringify(this.desired_image_properties)));
-  let scale_up = Math.exp(v);
-  // console.log(`v: ${v}, offset_x: ${offset_x}, offset_y: ${offset_y}, scale_up: ${scale_up}`)
-
-  this.zoom_level += v;
-  // console.log(this.zoom_level);
-  this.desired_image_properties.x += offset_x * (scale_up - 1) / scale_up;
-  this.desired_image_properties.y += offset_y * (scale_up - 1) / scale_up;
-  // console.log(v)
-  this.desired_image_properties.pix_x = this.pix_size_x;
-  this.desired_image_properties.pix_y = this.pix_size_y;
-  console.log(this.desired_image_properties)
-  if (this.flag) {
-    this.updateImage();
-  }
-}
-/**
- * Function responsible for the drag feature 
- */
-panBy(horiz_pix, vert_pix) {
-  this.desired_image_properties.x -= horiz_pix * this.pix_size_x;
-  this.desired_image_properties.y -= vert_pix  * this.pix_size_y;
-  if (this.flag) {
-    this.updateImage();
-  }
-}
-
-/**
- * It will call After the present image finish loading
- */
-updateImage() {
-  if (!(JSON.stringify(this.present_image_properties).toLowerCase() === JSON.stringify(this.desired_image_properties).toLowerCase())) {
+  ngOnInit() {
+    this.set(0, 0, 0.015625, 0.015625, 256, 256, 1225, "cpp", true);
     this.setParmeter();
-    this.flag = false
-    this.present_image_properties = (JSON.parse(JSON.stringify(this.desired_image_properties)));
+    // Make images interactive.
+    this.imageInteractive();
   }
-  else {
-    this.flag = true;
-  }
-}
-endDrag() {
-  setTimeout(() => { this.dragging = false; }, 0);
-}
 
+
+  //set the parameter to the desired_image_properties object
+  set(x, y, pix_x, pix_y, width, height, max_depth, renderer, darken) {
+    this.desired_image_properties = {
+      x: x,
+      y: y,
+      pix_x: pix_x,
+      pix_y: pix_y,
+      width: width,
+      height: height,
+      max_depth: max_depth,
+      renderer: renderer,
+      darken: darken
+    }
+    this.present_image_properties = (JSON.parse(JSON.stringify(this.desired_image_properties)));
+
+  }
+  /**
+   * setter and getter method
+   */
+
+  set zoom_level(v) { this._zoom_level = v; }
+  get zoom_level() { return this._zoom_level; }
+
+  set scale(v) { this.zoom_level = Math.log(v); }
+  get scale() { return Math.exp(this.zoom_level); }
+
+  // Scale of 1.0 has size_y of 4.0.
+  get image_size_x() { return this.desired_image_properties.pix_x * this.desired_image_properties.width; }
+  get image_size_y() { return 4 / this.scale; }
+
+  // Pixel x/y are same, based on height.
+  get pix_size_x() { return this.pix_size_y; }
+  get pix_size_y() { return this.image_size_y / this.desired_image_properties.height; }
+
+  /**
+   * sets the parameter to the image src
+   */
+  setParmeter() {
+    var json = JSON.stringify(this.desired_image_properties);
+    var img_url = "http://127.0.1.1:8888/img?json=" + json;
+    // console.log(json)
+    console.log(img_url)
+    $(".image").attr("src", img_url);
+  }
+  /**
+   * Function that responsible for image-interaction
+   */
+  imageInteractive() {
+    interact(".image")
+      .draggable({
+        inertia: true
+      })
+      .on('down', (e) => {
+        // Set flag `dragging`.
+        this.dragging = true;
+        if (this.motion !== "position") {
+          console.log(1)
+          //@ts-ignore
+          this.motion_x = e.offsetX - e.target.clientWidth / 2.0;
+          //@ts-ignore
+          this.motion_y = e.offsetY - e.target.clientHeight / 2.0;
+          this.motion_z = 0;
+          this.velocity_x = 0;
+          this.velocity_y = 0;
+          this.velocity_z = 0;
+          this.setMotionTimeout();
+        }
+      })
+      .on('up', (e) => {
+        this.endDrag();
+      })
+      .on('dragstart', (e) => {
+        this.dragging = true;
+      })
+      .on('dragend', (e) => {
+        // Clear flag `dragging` to re-enable mouseclicks.
+        // Uses setTimeout to ensure that actions currently firing (on mouseup at the end of a drag)
+        // are still blocked from changing the content.
+        this.endDrag();
+      })
+      .on('dragmove', (e) => {
+        // this.debugMessage(`DragMove: e = ${e}`);
+        // e.preventDefault();
+        if ((e.buttons & this.ZOOM_BUTTON_MASK) != 0) {
+          // Mouse button & drag used for zoom (not currently enabled).
+          let amt = e.dy / this.ZOOM_SLUGGISHNESS;
+          if (this.motion === "position") {
+            this.desired_image_properties.zoomBy(amt);
+          }
+        } else {
+          // Default for position.
+          let dx = e.dx;
+          let dy = e.dy;
+          this.motion_x += dx;
+          this.motion_y += dy;
+          if (this.motion === "position") {
+            this.panBy(dx, dy);
+
+          }
+        }
+      })
+      //@ts-ignore
+      .on("wheel", (e) => {
+        e.preventDefault();
+        // console.log(e)
+        let sluggishness;
+        if (e.originalEvent.deltaMode == 0) {
+          sluggishness = this.WHEEL_ZOOM_SLUGGISHNESS_PIXELS;
+        }
+        let amt = - e.originalEvent.deltaY / sluggishness;
+        if (this.motion === "position") {
+          this.zoomByAt(amt, (e.offsetX - e.target.clientWidth / 2.0) * this.desired_image_properties.pix_x,
+            (e.offsetY - e.target.clientHeight / 2.0) * this.desired_image_properties.pix_y);
+        }
+      })
+
+  }
+  zoomByAt(v, offset_x, offset_y) {
+    console.log(1)
+    this.present_image_properties = (JSON.parse(JSON.stringify(this.desired_image_properties)));
+    let scale_up = Math.exp(v);
+    // console.log(`v: ${v}, offset_x: ${offset_x}, offset_y: ${offset_y}, scale_up: ${scale_up}`)
+
+    this.zoom_level += v;
+    // console.log(this.zoom_level);
+    this.desired_image_properties.x += offset_x * (scale_up - 1) / scale_up;
+    this.desired_image_properties.y += offset_y * (scale_up - 1) / scale_up;
+    // console.log(v)
+    this.desired_image_properties.pix_x = this.pix_size_x;
+    this.desired_image_properties.pix_y = this.pix_size_y;
+    console.log(this.desired_image_properties)
+    if (this.flag) {
+      this.updateImage();
+    }
+  }
+  /**
+   * Function responsible for the drag feature 
+   */
+  panBy(horiz_pix, vert_pix) {
+    this.desired_image_properties.x -= horiz_pix * this.pix_size_x;
+    this.desired_image_properties.y -= vert_pix * this.pix_size_y;
+    if (this.flag) {
+      this.updateImage();
+    }
+  }
+
+  /**
+   * It will call After the present image finish loading
+   */
+  updateImage() {
+    if (!(JSON.stringify(this.present_image_properties).toLowerCase() === JSON.stringify(this.desired_image_properties).toLowerCase())) {
+      this.setParmeter();
+      this.flag = false
+      this.present_image_properties = (JSON.parse(JSON.stringify(this.desired_image_properties)));
+    }
+    else {
+      this.flag = true;
+    }
+  }
+  endDrag() {
+    setTimeout(() => { this.dragging = false; }, 0);
+  }
+
+  setMotionTimeout() {
+    setTimeout(() => { this.applyMotion(); }, this.MOTION_TIMEOUT);
+  }
+  applyMotion() {
+    if (this.dragging) {
+      
+        this.velocity_x = this.motion_x * - this.VELOCITY_FACTOR;
+        this.velocity_y = this.motion_y * - this.VELOCITY_FACTOR;
+        this.velocity_z = this.motion_z * this.ZOOM_VELOCITY_FACTOR;
+      
+      this.panBy(this.velocity_x, this.velocity_y);
+      this.setMotionTimeout();
+    }
+  }
 
 }
 
